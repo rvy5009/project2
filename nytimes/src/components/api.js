6ec7550ca60e9490c8d3f99e19e5fbe8727a782e
rvy5009 <rvy5009@gmail.com>
2019-12-13 15:03:51 -0500

    finished better readme

---
+++ nytimes/src/components/api.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+let source = "sports"
+let apiKey =  "dd76a03ead794c4e9e9f4c39be2b286e"
+let news = "https://newsapi.org/v2/everything?q=sports&apiKey=dd76a03ead794c4e9e9f4c39be2b286e"
+let apiUrl = `https://newsapi.org/v2/everything?q=${source}&from=2019-11-30&sortBy=publishedAt&apiKey=dd76a03ead794c4e9e9f4c39be2b286e`
+
+export const fetchMain = async () => {
+  let data = axios.get(apiUrl)
+  return data
+
+}
---
+++ nytimes/src/components/mainpage.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { fetchMain } from "./api"
+
+class MainPage extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    )
+  }
+}
+export default MainPage
